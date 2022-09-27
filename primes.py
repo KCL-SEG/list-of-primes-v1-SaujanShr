1316be73347e9d7dec5ede0ec5871ea729375f75
Shrestha, Saujan (k21075139) <saujan.shrestha@kcl.ac.uk>
2022-09-27 18:48:01 +0100

    Add function to the primes generator

--- primes.py
+++ primes.py
@@ -1,6 +1,17 @@
 """List of prime numbers generator."""
-"""ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
     list = []
+
+    if number_of_primes < 2:
+        return list
+
+    prime = [True for _ in range(number_of_primes + 1)]
+
+    for i in range(2, number_of_primes + 1):
+        if prime[i]:
+            list.append(i)
+            for j in range(i*i, number_of_primes, i):
+                prime[j] = False
+
     return list
