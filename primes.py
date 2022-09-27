ef0c9797f1b90863878925d2571940e6878382d7
Shrestha, Saujan (k21075139) <saujan.shrestha@kcl.ac.uk>
2022-09-27 19:39:53 +0100

    Add functionality to the primes function

--- primes.py
+++ primes.py
@@ -1,17 +1,19 @@
 """List of prime numbers generator."""
 
+from math import sqrt, floor
+
 def primes(number_of_primes):
-    list = []
+    list = [2]
+    i = 1
+    count = 1
 
-    if number_of_primes < 2:
-        return list
-
-    prime = [True for _ in range(number_of_primes + 1)]
-
-    for i in range(2, number_of_primes + 1):
-        if prime[i]:
+    while count < number_of_primes:
+        i += 2
+        for k in range(2, 1+floor(sqrt(i+1))):
+            if i%k == 0:
+                break
+        else:
             list.append(i)
-            for j in range(i*i, number_of_primes, i):
-                prime[j] = False
+            count += 1
 
     return list
